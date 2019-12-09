f8860c6c2f042e3d0413b10a682f70d6b88a28bc
Nory Lazo <nlazo@icloud.com>
2019-12-09 00:33:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,15 @@
 require 'pry'
 
-class Anagram
+# class Anagram
   
-  attr_accessor :word 
+#   attr_accessor :word 
   
-  def initialize(word)
-    @word = word
-  end 
+#   def initialize(word)
+#     @word = word
+#   end 
   
-  def match(array)
-    array.select { |array| @word.split("").sort == array.split("").sort}
-  end
+#   def match(array)
+#     array.select { |array| @word.split("").sort == array.split("").sort}
+#   end
   
-end 
+# end 
